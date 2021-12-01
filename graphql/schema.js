128e1d93f85b7f294cc51f57b7cf2acb68088e9c
snoopysecurity <sams@snyk.io>
2021-12-01 22:07:12 +0000

    feat: add graphql mutation queries

--- graphql/schema.js
+++ graphql/schema.js
@@ -1,12 +1,19 @@
 const graphql = require('graphql');
 const _ = require('lodash')
-
-const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLBoolean, GraphQLList, GraphQLSchema } = graphql;
+const jwt = require('jsonwebtoken')
+var fsPromise = require("fs").promises;
+const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLBoolean, GraphQLList, GraphQLSchema, GraphQLNonNull } = graphql;
 
 
 const Note = require('../models/notebook');
 const User = require('../models/users');
 
+const options = {
+  expiresIn: '2d',
+  issuer: 'https://github.com/snoopysecurity',
+  algorithms: ["HS256", "none"],
+  ignoreExpiration: true
+};
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -27,10 +34,20 @@ const NoteType = new GraphQLObjectType({
       body: { type: GraphQLString },
       created_date: { type: GraphQLString },
       user: { type: GraphQLString },
+      type: { type: new GraphQLList(GraphQLString)},
       no: { type: GraphQLString }
   })
 });
 
+
+const FileType = new GraphQLObjectType({
+  name: 'File',
+  fields: ( ) => ({
+      id: { type: GraphQLID },
+      filePath: { type: new GraphQLNonNull(GraphQLString) },
+      fileContent: { type: new GraphQLNonNull(GraphQLString) }
+  })
+});
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
@@ -53,7 +70,7 @@ const RootQuery = new GraphQLObjectType({
   args: {name:{type: GraphQLString}},
   async resolve(parent,args){
     note = args.name;
-    result = await Note.find({ name: note }, 'name body user _id').exec();
+    result = await Note.find({ name: note }, 'name body user _id type').exec();
     return result;
 
   } 
@@ -71,7 +88,6 @@ const RootQuery = new GraphQLObjectType({
       async resolve(parent,args){
         user = args.username;
         result = await User.find({ username: user }, '_id username admin').exec();
-        console.log(result)
         return result;
 
       } 
@@ -79,6 +95,67 @@ const RootQuery = new GraphQLObjectType({
   }
 });
 
+
+const Mutation = new GraphQLObjectType({
+  name: 'Mutations',
+  fields: {
+    createNote: {
+      type: new GraphQLList(NoteType),
+      args: {name:{type: GraphQLString},
+      body: { type: GraphQLString },
+      type: { type: new GraphQLList(GraphQLString)}},
+      async resolve(parent, args, req){
+    
+      let result = {}
+      try {
+        const token = req.headers.authorization.split(' ')[1]; // Bearer <token>
+        result = jwt.verify(token, process.env.JWT_SECRET, options);
+
+      } catch (error) {
+        throw new Error( "Missing JWT Auth Token");
+      }
+   
+        var new_note = new Note({ name: args.name , body: args.body, type: args.type, user: result.user});
+        new_note.save(function (err, note) {
+          if (err) {
+            throw new Error(err);
+          }
+        }); 
+        result = await Note.find({ name: args.name }, 'name body user _id type').exec();
+        return result;
+
+      }
+      },
+      updateUserUploadFile: {
+        type: FileType,
+        args: {
+          filePath: { type: new GraphQLNonNull(GraphQLString) },
+          fileContent: { type: new GraphQLNonNull(GraphQLString) },
+      },
+      async resolve(parent, args, req){
+
+        try {
+          const token = req.headers.authorization.split(' ')[1]; // Bearer <token>
+          result = jwt.verify(token, process.env.JWT_SECRET, options);
+  
+        } catch (error) {
+          throw new Error( "Missing JWT Admin Auth Token");
+        }
+
+        filePath = __dirname + '/../public/uploads/' + result.user + "/" +  args.filePath;
+        return await fsPromise.writeFile(filePath, args.fileContent);
+
+      }
+    }
+
+  }
+
+
+
+})
+
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation: Mutation
 });
+
